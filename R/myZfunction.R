591524290eacf42a4de5b6d19ccf12cab63576a2
Alberto Liu <al.10549329@gmail.com>
2020-02-14 18:29:27 -0600

    adding myBinomial function that calculates bionomial distribution

--- R/myZfunction.R
+++ R/myZfunction.R
@@ -14,3 +14,31 @@ myZfunction = function(x){
   z = (x - mean(x))/sd(x)
   z
 }
+#' myBinomial
+#' calculates the binomial distribution
+#' @param iter an integer
+#' @param n  an integer
+#' @param p a double
+#'
+#' @return binomial distribution
+#' @export
+#'
+#' @examples iter = 10, n = 3, p = 0.5; mybin(iter,n,p);
+mybin=function(iter=100,n=10, p=0.5){
+  # make a matrix to hold the samples
+  #initially filled with NA's
+  sam.mat=matrix(NA,nr=n,nc=iter, byrow=TRUE)
+  #Make a vector to hold the number of successes in each trial
+  succ=c()
+  for( i in 1:iter){
+    #Fill each column with a new sample
+    sam.mat[,i]=sample(c(1,0),n,replace=TRUE, prob=c(p,1-p))
+    #Calculate a statistic from the sample (this case it is the sum)
+    succ[i]=sum(sam.mat[,i])
+  }
+  #Make a table of successes
+  succ.tab=table(factor(succ,levels=0:n))
+  #Make a barplot of the proportions
+  barplot(succ.tab/(iter), col=rainbow(n+1), main="Binomial simulation", xlab="Number of successes")
+  succ.tab/iter
+}
