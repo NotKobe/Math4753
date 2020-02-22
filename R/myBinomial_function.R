0ea450210fe69840efe5a536624da39880e9f106
Alberto Liu <al.10549329@gmail.com>
2020-02-21 20:53:55 -0600

    adding myNcurve function that shows normal curve

--- R/myBinomial_function.R
+++ R/myBinomial_function.R
@@ -1,19 +1,3 @@
-#' My Function producing z values
-#'
-#' Produce vectors of z values
-#'
-#'
-#'
-#' @param x a vector
-#'
-#' @return a list of vectors
-#' @export
-#'
-#' @examples x = 1:7; myZfunction(x)
-myZfunction = function(x){
-  z = (x - mean(x))/sd(x)
-  z
-}
 #' myBinomial
 #' calculates the binomial distribution
 #' @param iter an integer
